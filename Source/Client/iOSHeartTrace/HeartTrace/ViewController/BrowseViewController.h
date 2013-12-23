080d710b935a4c140c04aaeca13b023814890cbe
taojigu <user@localhost.(none)>
2013-12-23 18:29:12 +0800

    add view controllers and localizedString

---
+++ Source/Client/iOSHeartTrace/HeartTrace/ViewController/BrowseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrowseViewController.h
+//  HeartTrace
+//
+//  Created by user on 13-12-23.
+//  Copyright (c) 2013年 voole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrowseViewController : UIViewController
+
+@end
