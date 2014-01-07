c226bc239443800a7f84554b807b6c838ba8f569
taojigu <user@localhost.(none)>
2014-01-07 17:20:19 +0800

    Finish Product button in WishWall

---
+++ Source/Client/iOSHeartTrace/HeartTrace/Configure/ImageNameConst.h
@@ -0,0 +1,13 @@
+//
+//  ImageNameConst.h
+//  HeartTrace
+//
+//  Created by user on 14-1-7.
+//  Copyright (c) 2014年 voole. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageNameConst : NSObject
+
+@end
